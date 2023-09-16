9edca7cf3f0d19b39a0a58ea789fea206133b39b
Agent47-tht <107775610+Agent47-tht@users.noreply.github.com>
2023-09-16 16:43:15 +0300

    main.py

--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from plyer import notification
 notification_title = "Merhaba bu test başlığıdır."
 notification_description = "Hmm bunu okuduğuna göre demekki kod çalışıyor.. niceee"
 
-while True:
-    notification.notify(
-        title = notification_title,
-        message = notification_description,
-        app_icon = None,
-        timeout = 0.4,
-        toast = False
-    )
+notification.notify(
+    title = notification_title,
+    message = notification_description,
+    app_icon = None,
+    timeout = 5,
+    toast = False
+)
